af06deea0e2b929e8816a34c232f70aed5047016
Franklin Shera <fshera96@gmail.com>
2022-12-09 05:13:47 +0300

    scss

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,145 +1,178 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
-import { Link } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <div>
-      <h1>
-        Welcome to Qwik <span class="lightning">⚡️</span>
-      </h1>
+    <main id="HomePage">
+      <section
+        class="showcase scecomm-container"
+        style="background: url('{{ asset('storage/images/showcase.webp')}}') no-repeat  center"
+      >
+        <div class="showcase-content">
+          <h2>Office & School Stationery, IT & Furniture</h2>
+          <p>Everything you need to operate seamlessly</p>
+          <button>BROWSE</button>
+        </div>
+      </section>
 
-      <ul>
-        <li>
-          Check out the <code>src/routes</code> directory to get started.
-        </li>
-        <li>
-          Add integrations with <code>npm run qwik add</code>.
-        </li>
-        <li>
-          More info about development in <code>README.md</code>
-        </li>
-      </ul>
-
-      <h2>Commands</h2>
-
-      <table class="commands">
-        <tr>
-          <td>
-            <code>npm run dev</code>
-          </td>
-          <td>Start the dev server and watch for changes.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run preview</code>
-          </td>
-          <td>Production build and start preview server.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run build</code>
-          </td>
-          <td>Production build.</td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add</code>
-          </td>
-          <td>Select an integration to add.</td>
-        </tr>
-      </table>
-
-      <h2>Add Integrations</h2>
-
-      <table class="commands">
-        <tr>
-          <td>
-            <code>npm run qwik add cloudflare-pages</code>
-          </td>
-          <td>
-            <a href="https://developers.cloudflare.com/pages" target="_blank">
-              Cloudflare Pages Server
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add express</code>
-          </td>
-          <td>
-            <a href="https://expressjs.com/" target="_blank">
-              Nodejs Express Server
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add netlify-edge</code>
-          </td>
-          <td>
-            <a href="https://docs.netlify.com/" target="_blank">
-              Netlify Edge Functions
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <code>npm run qwik add static</code>
-          </td>
-          <td>
-            <a
-              href="https://qwik.builder.io/qwikcity/static-site-generation/overview/"
-              target="_blank"
+      <section class="featured products-view scecomm-container">
+        <div class="title-action">
+          <h3>Featured Products</h3>
+          <a href="{{ route('collections.featured') }}">
+            View All
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              Static Site Generation (SSG)
-            </a>
-          </td>
-        </tr>
-      </table>
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </a>
+        </div>
 
-      <h2>Community</h2>
+        <div class="product-list">
+          {/* @foreach($products as $product)
 
-      <ul>
-        <li>
-          <span>Questions or just want to say hi? </span>
-          <a href="https://qwik.builder.io/chat" target="_blank">
-            Chat on discord!
+    @livewire('product-tile' ,['product' => $product])
+
+    @endforeach */}
+        </div>
+      </section>
+
+      <section class="shop-by-category scecomm-container">
+        <div class="title-action">
+          <h3>Shop by Category</h3>
+          <a href="{{ route('collections.categories') }}">
+            View All
+            <svg
+              class="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
           </a>
-        </li>
-        <li>
-          <span>Follow </span>
-          <a href="https://twitter.com/QwikDev" target="_blank">
-            @QwikDev
-          </a>
-          <span> on Twitter</span>
-        </li>
-        <li>
-          <span>Open issues and contribute on </span>
-          <a href="https://github.com/BuilderIO/qwik" target="_blank">
-            GitHub
-          </a>
-        </li>
-        <li>
-          <span>Watch </span>
-          <a href="https://qwik.builder.io/media/" target="_blank">
-            Presentations, Podcasts, Videos, etc.
-          </a>
-        </li>
-      </ul>
-      <Link class="mindblow" href="/flower/">
-        Blow my mind 🤯
-      </Link>
-    </div>
+        </div>
+
+        <div class="category-list">
+          <div class="category">
+            <img
+              src="storage/images/categories/copypaper_300x.webp"
+              alt="Printer & Copier Paper"
+            />
+
+            <h3>Printer & Copier Paper</h3>
+          </div>
+
+          <div class="category">
+            <img
+              src="storage/images/categories/binders_300x.webp"
+              alt="Files & Folders"
+            />
+
+            <h3>Files & Folders</h3>
+          </div>
+
+          <div class="category">
+            <img
+              src="storage/images/categories/hp-ink_300x.webp"
+              alt="Toners & Catriges"
+            />
+
+            <h3>Toners & Catriges</h3>
+          </div>
+
+          <div class="category">
+            <img
+              src="storage/images/categories/officechair_300x.webp"
+              alt="Chairs"
+            />
+
+            <h3>Chairs</h3>
+          </div>
+
+          <div class="category">
+            <img
+              src="storage/images/categories/Pen_300x.webp"
+              alt="Pens & Ink"
+            />
+
+            <h3>Pens & Ink</h3>
+          </div>
+
+          <div class="category">
+            <img
+              src="storage/images/categories/School_Supplies_300x.webp"
+              alt="Its Back to SChool 2021..."
+            />
+
+            <h3>Its Back to SChool 2021...</h3>
+          </div>
+        </div>
+      </section>
+
+      <section class="our-assurances scecomm-container">
+        <div class="assurance">
+          <i class="ti-credit-card"></i>
+          <div class="claim">
+            <h3>Secure Payments</h3>
+            <p>M-Pesa or Cash on Pickup</p>
+          </div>
+        </div>
+
+        <div class="assurance">
+          <i class="ti-medall"></i>
+          <div class="claim">
+            <h3>100% Authentic</h3>
+            <p>Guaranteed high quality with genuine brands</p>
+          </div>
+        </div>
+
+        <div class="assurance">
+          <i class="ti-headphone-alt"></i>
+          <div class="claim">
+            <h3>Top-Notch Support</h3>
+            <p>Over 27 years of excellent customer satisfaction</p>
+          </div>
+        </div>
+
+        <div class="assurance">
+          <i class="ti-package"></i>
+          <div class="claim">
+            <h3>Reliable delivery</h3>
+            <p>Fast and on-time deliveries to keep you going</p>
+          </div>
+        </div>
+      </section>
+
+      <section class="divider-section scecomm-container">
+        <hr />
+      </section>
+    </main>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "Welcome to Qwik",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content: "Qwik site description",
     },
   ],
 };
