c4a1469fa70989cd212247319acc8e1d8a246eca
Franklin Shera <fshera96@gmail.com>
2022-12-09 22:28:43 +0300

    mamba ui cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,68 +1,41 @@
 import { component$ } from "@builder.io/qwik";
+import CartItem from "./CartItem";
 
 export default component$(() => {
   return (
-    <div class="cart-wrapper">
-      <section class="shopping-cart">
-        <div class="cart-header">
-          <h6>
-            Shopping Cart <span>3 Items</span>
-          </h6>
-          <a href="/" class="full-cart">
-            {" "}
-            Full View
-            <svg
-              class="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M14 5l7 7m0 0l-7 7m7-7H3"
-              ></path>
-            </svg>
-          </a>
+    <div class="fixed top-0 left-0 flex justify-center pt-10 [filter:blur(2px)] w-full h-full z-50 ">
+      <div class="flex flex-col max-w-3xl p-6 space-y-4 sm:p-10 rounded drop-shadow bg-gray-50 text-gray-800">
+        <h2 class="text-xl font-semibold">Your cart</h2>
+        <ul class="flex flex-col divide-y divide-gray-700">
+          {[...new Array(3).fill(undefined)].map((cartItem) => (
+            <CartItem />
+          ))}
+        </ul>
+        <div class="space-y-1 text-right">
+          <p>
+            Total amount:
+            <span class="font-semibold">357 €</span>
+          </p>
+          <p class="text-sm text-gray-400">
+            Not including taxes and shipping costs
+          </p>
         </div>
-
-        <div class="cart-items ">
-          <div class="labels">
-            <span>Image</span>
-            <span>Product</span>
-            <span>Qty</span>
-            <span>Price</span>
-          </div>
-          <ul>
-            {/* @foreach( $cart as $cartItem) */}
-
-            <li>
-              <div class="glimpse">Baking Soda</div>
-
-              <div class="product-image">
-                <img src="{{ asset('storage/images/products/product' . rand(1,6) .'.webp')  }}" />
-              </div>
-              <span class="title">
-                <a href="{{ route('collections.product-view',['product' => $cartItem,'productSlug' => $cartItem->slug]) }}">
-                  Title here...{" "}
-                </a>
-              </span>
-              <span class="quantity pl-1">5</span>
-              <span class="price ">488 Ksh</span>
-            </li>
-
-            {/* @endforeach */}
-          </ul>
+        <div class="flex justify-end space-x-4">
+          <button
+            type="button"
+            class="px-6 py-2 border rounded-md border-violet-400"
+          >
+            Back
+            <span class="sr-only sm:not-sr-only">to shop</span>
+          </button>
+          <button
+            type="button"
+            class="px-6 py-2 border rounded-md bg-violet-400 text-gray-900 border-violet-400"
+          >
+            <span class="sr-only sm:not-sr-only">Continue to</span>Checkout
+          </button>
         </div>
-
-        <div class="action">
-          <div class="total">Total 1,000 Ksh.</div>
-
-          <button>Checkout</button>
-        </div>
-      </section>
+      </div>
     </div>
   );
 });
