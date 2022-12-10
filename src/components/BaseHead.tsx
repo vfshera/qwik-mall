c2dd0431cb4813c700c6c2fea491a11b0ae77e83
Franklin Shera <fshera96@gmail.com>
2022-12-10 04:38:18 +0300

    data

--- src/components/BaseHead.tsx
+++ src/components/BaseHead.tsx
@@ -1,5 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
+import { SITE } from "~/data/config";
 
 /**
  * The BaseHead component is placed inside of the document `<head>` element.
@@ -10,7 +11,9 @@ export default component$(() => {
 
   return (
     <>
-      <title>{head.title}</title>
+      <title>
+        {head.title} - {SITE.name}
+      </title>
 
       <link rel="canonical" href={loc.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
