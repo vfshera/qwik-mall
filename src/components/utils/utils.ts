cab8960bd68952a810c3904524b141e8cb139f35
Franklin Shera <fshera96@gmail.com>
2022-12-10 05:16:42 +0300

    more utils and data

---
+++ src/components/utils/utils.ts
@@ -0,0 +1,9 @@
+export const slugify = (value: string): string => {
+  return value
+    .normalize("NFD") // split an accented letter in the base letter and the acent
+    .replace(/[\u0300-\u036f]/g, "") // remove all previously split accents
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9 ]/g, "") // remove all chars not letters, numbers and spaces (to be replaced)
+    .replace(/\s+/g, "-"); // separator
+};
--- src/data/config.ts
+++ src/data/config.ts
@@ -1,3 +1,4 @@
 export const SITE = {
   name: "Qwik Mall",
+  phone: "+254712345678",
 };
