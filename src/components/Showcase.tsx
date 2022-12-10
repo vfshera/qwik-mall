80f2557aefb1f32e4fa1a8982c84cc0037b74ed4
Franklin Shera <fshera96@gmail.com>
2022-12-10 04:39:04 +0300

    showcase

---
+++ src/components/Showcase.tsx
@@ -0,0 +1,17 @@
+import { component$ } from "@builder.io/qwik";
+import { SITE } from "~/data/config";
+
+export default component$(() => {
+  return (
+    <section
+      class="showcase scecomm-container bg-no-repeat h-[450px]"
+      style="background: url('/images/hero.webp')"
+    >
+      <div class="flex flex-col justify-center items-center gap-5 h-full">
+        <h2 class="text-white text-5xl font-semibold">
+          Welcome to {SITE.name}
+        </h2>
+      </div>
+    </section>
+  );
+});
