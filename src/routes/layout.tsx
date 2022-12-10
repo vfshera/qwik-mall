5ac5c1035b421cadcddd2421182e24f8ba8ed0ac
Franklin Shera <fshera96@gmail.com>
2022-12-11 02:32:24 +0300

    use context

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,8 +1,10 @@
-import { component$, Slot } from "@builder.io/qwik";
+import { component$, Slot, useContextProvider } from "@builder.io/qwik";
 import Footer from "~/components/Footer";
 import Header from "~/components/Header";
+import { GlobalContext, initialState } from "~/GlobalState";
 
 export default component$(() => {
+  useContextProvider(GlobalContext, initialState);
   return (
     <>
       <main>
