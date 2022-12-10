161109bdabcc5fd26c0dd023a21aca584211bab9
Franklin Shera <fshera96@gmail.com>
2022-12-10 05:55:51 +0300

    links

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { component$ } from "@builder.io/qwik";
+import { slugify } from "./utils/utils";
 
 interface Props {
   product: Product;
@@ -6,7 +7,9 @@ interface Props {
 export default component$(({ product }: Props) => {
   return (
     <a
-      href="#"
+      href={`/products/${slugify(product.category)}/${product.id}/${slugify(
+        product.title
+      )}`}
       class="relative flex flex-col rounded-md shadow-sm  overflow-hidden group"
     >
       <button class="absolute right-4 top-4 z-10 rounded-full bg-white hover:bg-pink-500 p-1.5 text-gray-900  hover:text-white transition-colors">
@@ -31,22 +34,22 @@ export default component$(({ product }: Props) => {
       <img
         src={product.image}
         alt={product.title}
-        class="object-cover w-full h-64 transition duration-500 group-hover:scale-105 sm:h-72"
+        class="object-cover w-full h-48 md:h-56 lg:h-64 transition duration-500 group-hover:scale-105 sm:h-72"
       />
 
-      <div class="relative p-6 flex-1 flex flex-col justify-between bg-white border   border-gray-100">
-        <span class="whitespace-nowrap bg-brand-1 w-max text-white px-3 py-1.5 text-xs font-medium">
+      <div class="relative p-4 md:p-6 flex-1 flex flex-col justify-between bg-white border   border-gray-100">
+        <span class="whitespace-nowrap bg-brand-1 w-max text-white px-3 py-1 md:py-1.5 text-xs font-medium">
           New
         </span>
 
-        <p class="mt-4 text-lg font-medium text-gray-900 line-clamp-1">
+        <p class="mt-4 text-sm md:text-lg font-medium text-gray-900 line-clamp-1">
           {product.title}
         </p>
 
         <p class="mt-1.5 text-sm text-gray-700">${product.price}</p>
 
         <form class="mt-4">
-          <button class="block w-full p-4 text-sm font-medium transition bg-brand-1 text-white rounded hover:scale-105">
+          <button class="block w-full py-2.5  md:p-4 text-sm font-medium transition bg-brand-1 text-white rounded hover:scale-105">
             Add to Cart
           </button>
         </form>
