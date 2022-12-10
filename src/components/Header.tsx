4eb4408c5f54d391454ead9f5e4a8d27be565802
Franklin Shera <fshera96@gmail.com>
2022-12-10 06:32:06 +0300

    clean

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -55,46 +55,21 @@ export default component$(() => {
               />
             </svg>
           </button>
-          {/* @if (Route::has('login')) */}
           <div class="space-x-4 self-end">
-            {/* @auth */}
-            {/* <a class="font-bold" href="{{ route('dashboard') }}">
-              Username
-            </a> */}
-            {/* <a
-              href="{{ route('logout') }}"
-              class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
-            >
-              Log out
-            </a> */}
-
-            <form
-              id="logout-form"
-              action="{{ route('logout') }}"
-              method="POST"
-              style="display: none;"
-            >
-              {/* @csrf */}
-            </form>
-            {/* @else */}
             <a
-              href="{{ route('login') }}"
+              href="#"
               class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
             >
               Log in
             </a>
 
-            {/* @if (Route::has('register')) */}
             <a
-              href="{{ route('register') }}"
+              href="#"
               class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
             >
               Register
             </a>
-            {/* @endif
-        @endauth */}
           </div>
-          {/* @endif */}
         </ul>
       </nav>
 
